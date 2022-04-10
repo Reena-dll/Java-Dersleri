34d6f84db6419222e2973206ccbe72f69b5fda9f
Oğuzhan Sadıkoğlu <58795753+Reena-dll@users.noreply.github.com>
2022-04-10 18:50:38 +0300

    uploaded
    uploaded

---
+++ 087 - Palindromik Kelimeler/src/main.java
@@ -0,0 +1,33 @@
+public class main {
+
+    static boolean isPalindrom(String str) {
+
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("kavak"));
+    }
+}
